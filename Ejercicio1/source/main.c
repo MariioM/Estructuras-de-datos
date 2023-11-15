548d939263d7c073a65b2ae3a95dd7027015e2ab
Mario Martínez Vitutia <9205625@alumnos.ufv.es>
2023-11-15 18:53:56 +0100

    LeerFichero started

--- Ejercicio1/source/main.c
+++ Ejercicio1/source/main.c
@@ -5,12 +5,11 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
-//#include "tools/Gen.h"
+// #include "tools/Gen.h"
 #include "tools/Pila.h"
 #include "tools/Err.h"
 #include "tools/Cola.h"
 #include "tools/Lista.h"
-
 
 #define OPCION_UNO 1
 #define OPCION_DOS 2
@@ -21,65 +20,114 @@
 #define OPCION_SIETE 7
 #define OPCION_OCHO 8
 
+#define NOM_FICHERO "fich02.csv"
+#define NUM_CHAR_FICH 50
 
-#define NOM_FICHERO "fich02Copia.csv"
-
-
-int LeeFichero (char *NomFichero, tElemento *p, int *Num);
+void CalculaUsuarios(FILE *pf_usuarios, int *num_usuarios);
+int LeeFichero(FILE *pf_usuarios, tElemento *p, int num_usuarios, char lineaDatos[], char Nombre[], char Apellido[], char Password[]);
 void Pausar(void);
 void ConstruirPilas(tPila *pPila1, tPila *pPila2, tElemento *pElemento);
 tPila *ConstruirPila(tPila *pPila1, tElemento *pElemento);
-tCola ConstruirCola(tCola *pCola1,  tElemento *pElemento);
-tPila * ExtraerPilaOrden(tPila *pPila, char Letra);
+tCola ConstruirCola(tCola *pCola1, tElemento *pElemento);
+tPila *ExtraerPilaOrden(tPila *pPila, char Letra);
 tCola ExtraerColaOrden(tCola Cola, char Letra);
 
-main (void)
+int main(void)
 {
+    tPila *pPila1, *pPila2;
+    pPila1 = NULL;
+    pPila2 = NULL;
+    char Cadena[MAX_CAR * 3 + 2];
+    char *cpToken;
+    int FlgSalir;
+    int TipoOperacion;
+    int Num1, Num2, i;
+    int Res, Num;
+    tElemento e;
+    tElemento *pElemento;
+    FILE *pf_usuarios;
+    int num_usuarios;
+    int LongI, LatI;
+    char Nombre[MAX_CAR];
+    char Apellido[MAX_CAR];
+    char Password[MAX_CAR];
+    int k;
+    tCola Cola1;
+    tCola Cola2;
+    int j;
+    int Pos;
+    tElemento Elemento;
+    tLista pLista;
+    tPosicion pPos;
+    char Let;
 
-  tPila *pPila1, *pPila2;
-
-  pPila1=NULL;
-  pPila2=NULL;
-  char Cadena[MAX_CAR*3+2];
-  char *cpToken;
-  int FlgSalir;
-  int TipoOperacion;
-  int Num1, Num2,i;
-  int Res, Num;
-  tElemento e;
-  tElemento *pElemento;
-  int LongI, LatI;
-  char Nombre[MAX_CAR];
-  char Apellido[MAX_CAR];
-  char Password[MAX_CAR];
-  int k;
-  tCola Cola1;
-  tCola Cola2;
-  int j;
-  int Pos;
-  tElemento Elemento;
-  tLista pLista;
-  tPosicion pPos;
-  char Let;
-
-
-/* A rellenar por el alumno */
-
-
-  }
-
-
-
-
-
-int LeeFichero (char *NomFichero, tElemento *p, int *Num)
-{
-   
-/* A rellenar por el alumno */
-
-
+    /* Lectura del fichero */
+    //Se calcula el número de usuarios existentes
+    CalculaUsuarios(pf_usuarios, &num_usuarios);
+    //Se le asigna memoria dinámica al puntero que apunta a los usuarios
+    pElemento = (tElemento *) malloc(num_usuarios * sizeof(tElemento)); 
+    //Se lee y guarda el fichero
+    LeeFichero(pf_usuarios, pElemento, num_usuarios, Cadena, Nombre, Apellido, Password);
+    scanf("%d");
+  return 0;
 }
 
+void CalculaUsuarios(FILE *pf_usuarios, int *num_usuarios){
+    //Se define una variable de control
+    char control;
+    *num_usuarios = 1;
+    //Se abre el fichero donde se encuentran los datos
+    pf_usuarios = fopen("../data/fich02.csv", "r");
+    //Control de apertura fichero
+    if(pf_usuarios == NULL){
+        *num_usuarios = 0;
+        printf("Error al abrir el fichero.");
+        fclose(pf_usuarios);
+    }
+    //Se lee el numero de filas del fichero
+    while(control != EOF){
+        control = fgetc(pf_usuarios);
+        if(control == '\n'){
+          *num_usuarios += 1;
+        }
+    }
+    fclose(pf_usuarios);
+}
+
+int LeeFichero(FILE *pf_usuarios, tElemento *usuario, int num_usuarios, char lineaDatos[], char Nombre[], char Apellido[], char Password[])
+{
+    char delimiter[] = ";";
+    //Se abre el fichero
+    pf_usuarios = fopen("../data/fich02.csv", "r");
+    //Control de apertura fichero
+    if(pf_usuarios == NULL){
+        printf("Error al abrir el fichero.");
+        fclose(pf_usuarios);
+    }
+    do{
+        for(int i = 0; i < num_usuarios; i++){
+            fgets(lineaDatos, NUM_CHAR_FICH, pf_usuarios);
+            char *token = strtok(lineaDatos, delimiter);
+            if(i == 0){
+                strcmp(token ,usuario->Nombre);
+            }
+            else if(i == 1){
+                strcmp(token, usuario->Apellido);
+            }
+            else if(i == 2){
+                strcmp(token, usuario->Password);
+            }
+            else{
+              strcmp("Unknown", usuario->Nombre);
+            }
+        }
+        usuario++;
+    }while(!feof(pf_usuarios));
+    for(int i = 0; i < num_usuarios; i++){
+        printf("Nombre: %s Apellido: %s Password: %s", usuario->Nombre, usuario->Apellido, usuario->Password);
+        usuario ++;
+    }
+}
 
 void Pausar(void)
 {
@@ -89,47 +137,27 @@ void Pausar(void)
 void ConstruirPilas(tPila *pPila1, tPila *pPila2, tElemento *pElemento)
 {
 
- /*A rellenar por el alumno*/
+  /*A rellenar por el alumno*/
+}
 
-
- }
-
- 
 tPila *ConstruirPila(tPila *pPila1, tElemento *pElemento)
 {
 
- /* A rellenar por el alumno */
-
-
+  /* A rellenar por el alumno */
 }
 
-
-tCola ConstruirCola(tCola *pCola1,  tElemento *pElemento)
+tCola ConstruirCola(tCola *pCola1, tElemento *pElemento)
 {
-/* A rellenar por el alumno */
-
-
-
- }
-
-
-tPila * ExtraerPilaOrden(tPila *pPila, char Letra)
-{
-
-/* A rellenar por el alumno */
-
+  /* A rellenar por el alumno */
 }
 
+tPila *ExtraerPilaOrden(tPila *pPila, char Letra)
+{
+
+  /* A rellenar por el alumno */
+}
 
 tCola ExtraerColaOrden(tCola Cola, char Letra)
 {
-/* A rellenar por el alumno */
-
-
-
+  /* A rellenar por el alumno */
 }
-
-
-
-
-
