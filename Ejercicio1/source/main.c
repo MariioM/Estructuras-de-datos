49ecb14cb38a829831f77f1ec5957948ae160d53
Mario Martínez Vitutia <9205625@alumnos.ufv.es>
2023-11-20 18:03:20 +0100

    modified leer fichero

--- Ejercicio1/source/main.c
+++ Ejercicio1/source/main.c
@@ -24,7 +24,7 @@
 #define NUM_CHAR_FICH 50
 
 void CalculaUsuarios(FILE *pf_usuarios, int *num_usuarios);
-int LeeFichero(FILE *pf_usuarios, tElemento *p, int num_usuarios, char lineaDatos[], char Nombre[], char Apellido[], char Password[]);
+int LeeFichero(FILE *pf_usuarios, tElemento *p, int num_usuarios, char lineaDatos[]);
 void Pausar(void);
 void ConstruirPilas(tPila *pPila1, tPila *pPila2, tElemento *pElemento);
 tPila *ConstruirPila(tPila *pPila1, tElemento *pElemento);
@@ -34,99 +34,95 @@ tCola ExtraerColaOrden(tCola Cola, char Letra);
 
 int main(void)
 {
-    tPila *pPila1, *pPila2;
-    pPila1 = NULL;
-    pPila2 = NULL;
-    char Cadena[MAX_CAR * 3 + 2];
-    char *cpToken;
-    int FlgSalir;
-    int TipoOperacion;
-    int Num1, Num2, i;
-    int Res, Num;
-    tElemento e;
-    tElemento *pElemento;
-    FILE *pf_usuarios;
-    int num_usuarios;
-    int LongI, LatI;
-    char Nombre[MAX_CAR];
-    char Apellido[MAX_CAR];
-    char Password[MAX_CAR];
-    int k;
-    tCola Cola1;
-    tCola Cola2;
-    int j;
-    int Pos;
-    tElemento Elemento;
-    tLista pLista;
-    tPosicion pPos;
-    char Let;
+  tPila *pPila1, *pPila2;
+  pPila1 = NULL;
+  pPila2 = NULL;
+  char Cadena[MAX_CAR * 3 + 2];
+  char *cpToken;
+  int FlgSalir;
+  int TipoOperacion;
+  int Num1, Num2, i;
+  int Res, Num;
+  tElemento e;
+  tElemento *pElemento;
+  FILE *pf_usuarios;
+  int num_usuarios;
+  int LongI, LatI;
+  char Nombre[MAX_CAR];
+  char Apellido[MAX_CAR];
+  char Password[MAX_CAR];
+  int k;
+  tCola Cola1;
+  tCola Cola2;
+  int j;
+  int Pos;
+  tElemento Elemento;
+  tLista pLista;
+  tPosicion pPos;
+  char Let;
 
-    /* Lectura del fichero */
-    //Se calcula el número de usuarios existentes
-    CalculaUsuarios(pf_usuarios, &num_usuarios);
-    //Se le asigna memoria dinámica al puntero que apunta a los usuarios
-    pElemento = (tElemento *) malloc(num_usuarios * sizeof(tElemento)); 
-    //Se lee y guarda el fichero
-    LeeFichero(pf_usuarios, pElemento, num_usuarios, Cadena, Nombre, Apellido, Password);
-    scanf("%d");
+  /* Lectura del fichero */
+  // Se calcula el número de usuarios existentes
+  CalculaUsuarios(pf_usuarios, &num_usuarios);
+  // Se le asigna memoria dinámica al puntero que apunta a los usuarios
+  pElemento = (tElemento *)malloc(num_usuarios * sizeof(tElemento));
+  // Se lee y guarda el fichero
+  LeeFichero(pf_usuarios, pElemento, num_usuarios, Cadena);
+  scanf("%d");
   return 0;
 }
 
-void CalculaUsuarios(FILE *pf_usuarios, int *num_usuarios){
-    //Se define una variable de control
-    char control;
-    *num_usuarios = 1;
-    //Se abre el fichero donde se encuentran los datos
-    pf_usuarios = fopen("../data/fich02.csv", "r");
-    //Control de apertura fichero
-    if(pf_usuarios == NULL){
-        *num_usuarios = 0;
-        printf("Error al abrir el fichero.");
-        fclose(pf_usuarios);
-    }
-    //Se lee el numero de filas del fichero
-    while(control != EOF){
-        control = fgetc(pf_usuarios);
-        if(control == '\n'){
-          *num_usuarios += 1;
-        }
-    }
+void CalculaUsuarios(FILE *pf_usuarios, int *num_usuarios)
+{
+  // Se define una variable de control
+  char control;
+  *num_usuarios = 1;
+  // Se abre el fichero donde se encuentran los datos
+  pf_usuarios = fopen("../data/fich02.csv", "r");
+  // Control de apertura fichero
+  if (pf_usuarios == NULL)
+  {
+    *num_usuarios = 0;
+    printf("Error al abrir el fichero.");
     fclose(pf_usuarios);
+  }
+  // Se lee el numero de filas del fichero
+  while (control != EOF)
+  {
+    control = fgetc(pf_usuarios);
+    if (control == '\n')
+    {
+      *num_usuarios += 1;
+    }
+  }
+  fclose(pf_usuarios);
 }
 
-int LeeFichero(FILE *pf_usuarios, tElemento *usuario, int num_usuarios, char lineaDatos[], char Nombre[], char Apellido[], char Password[])
+int LeeFichero(FILE *pf_usuarios, tElemento *usuario, int num_usuarios, char lineaDatos[])
 {
-    char delimiter[] = ";";
-    //Se abre el fichero
-    pf_usuarios = fopen("../data/fich02.csv", "r");
-    //Control de apertura fichero
-    if(pf_usuarios == NULL){
-        printf("Error al abrir el fichero.");
-        fclose(pf_usuarios);
-    }
-    do{
-        for(int i = 0; i < num_usuarios; i++){
-            fgets(lineaDatos, NUM_CHAR_FICH, pf_usuarios);
-            char *token = strtok(lineaDatos, delimiter);
-            if(i == 0){
-                strcmp(token ,usuario->Nombre);
-            }
-            else if(i == 1){
-                strcmp(token, usuario->Apellido);
-            }
-            else if(i == 2){
-                strcmp(token, usuario->Password);
-            }
-            else{
-              strcmp("Unknown", usuario->Nombre);
-            }
-        }
-        usuario++;
-    }while(!feof(pf_usuarios));
-    for(int i = 0; i < num_usuarios; i++){
-        printf("Nombre: %s Apellido: %s Password: %s", usuario->Nombre, usuario->Apellido, usuario->Password);
-        usuario ++;
-    }
+  // Se abre el fichero
+  pf_usuarios = fopen("../data/fich02.csv", "r");
+  // Control de apertura fichero
+  if (pf_usuarios == NULL)
+  {
+    printf("Error al abrir el fichero.");
+    return -1; // Devuelve un valor para indicar error
+  }
+  for (int i = 0; i < num_usuarios; i++)
+  {
+    fscanf(pf_usuarios, "%s;%s;%s", usuario->Nombre, usuario->Apellido, usuario->Password);
+    usuario++;
+  }
+  fclose(pf_usuarios); // Cierra el archivo después de leer los datos
+
+  // Imprime los datos almacenados
+  printf("Datos almacenados:\n");
+  for (int i = 0; i < num_usuarios; i++)
+  {
+    printf("Nombre: %s\nApellido: %s\nPassword: %s\n", usuario->Nombre, usuario->Apellido, usuario->Password);
+    usuario++;
+  }
+  return 0; // Devuelve 0 para indicar éxito
 }
 
 void Pausar(void)
